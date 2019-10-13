75b64beb6938b2a67eef28cc8760a1591715d19a
Navindu Amarakoon <navanavi3@gmail.com>
2019-10-13 13:27:51 +0530

    SWs are a pain in the a

--- sw.js
+++ sw.js
@@ -1,64 +1,73 @@
-var CACHE_NAME = 'realtime-singlish';
-var version = "-v05.1"
-var urlsToCache = [
-  '/realtime-singlish/styles/main.css',
-  '/realtime-singlish/scripts/main.js',
-  '/realtime-singlish/index.html',
-  
-  
+var VERSION = 'realtime-english-v5.2';
+
+var cacheFirstFiles = [
+    '/realtime-singlish/styles/main.css',
+    '/realtime-singlish/scripts/main.js',
+    '/realtime-singlish/index.html',
+
 ];
 
+var networkFirstFiles = [
+  // ADDME: Add paths and URLs to pull from network first. Else fall back to cache if offline. Examples:
+  // 'index.html',
+  // 'build/build.js',
+  // 'css/index.css'
+];
 
-// Installing a sevice worker and defining files to be cached. 
-self.addEventListener('install', function(event) {
-  // Perform install steps
+// Below is the service worker code.
+
+var cacheFiles = cacheFirstFiles.concat(networkFirstFiles);
+
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME+version)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(VERSION).then(cache => {
+      return cache.addAll(cacheFiles);
+    })
   );
 });
 
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') { return; }
+  if (networkFirstFiles.indexOf(event.request.url) !== -1) {
+    event.respondWith(networkElseCache(event));
+  } else if (cacheFirstFiles.indexOf(event.request.url) !== -1) {
+    event.respondWith(cacheElseNetwork(event));
+  }
+  event.respondWith(fetch(event.request));
+});
 
-// Updating the service worker.
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request)
-        .then(function(response) {
-          // Cache hit - return response
-          if (response) {
-            return response;
-          }
-  
-          // IMPORTANT: Clone the request. A request is a stream and
-          // can only be consumed once. Since we are consuming this
-          // once by cache and once by the browser for fetch, we need
-          // to clone the response.
-          var fetchRequest = event.request.clone();
-  
-          return fetch(fetchRequest).then(
-            function(response) {
-              // Check if we received a valid response
-              if(!response || response.status !== 200 || response.type !== 'basic') {
-                return response;
-              }
-  
-              // IMPORTANT: Clone the response. A response is a stream
-              // and because we want the browser to consume the response
-              // as well as the cache consuming the response, we need
-              // to clone it so we have two streams.
-              var responseToCache = response.clone();
-  
-              caches.open(CACHE_NAME)
-                .then(function(cache) {
-                  cache.put(event.request, responseToCache);
-                });
-  
-              return response;
-            }
-          );
-        })
-      );
+// If cache else network.
+// For images and assets that are not critical to be fully up-to-date.
+// developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook/
+// #cache-falling-back-to-network
+function cacheElseNetwork (event) {
+  return caches.match(event.request).then(response => {
+    function fetchAndCache () {
+       return fetch(event.request).then(response => {
+        // Update cache.
+        caches.open(VERSION).then(cache => cache.put(event.request, response.clone()));
+        return response;
+      });
+    }
+
+    // If not exist in cache, fetch.
+    if (!response) { return fetchAndCache(); }
+
+    // If exists in cache, return from cache while updating cache in background.
+    fetchAndCache();
+    return response;
   });
+}
+
+// If network else cache.
+// For assets we prefer to be up-to-date (i.e., JavaScript file).
+function networkElseCache (event) {
+  return caches.match(event.request).then(match => {
+    if (!match) { return fetch(event.request); }
+    return fetch(event.request).then(response => {
+      // Update cache.
+      caches.open(VERSION).then(cache => cache.put(event.request, response.clone()));
+      return response;
+    }) || response;
+  });
+}
